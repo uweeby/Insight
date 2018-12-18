24f91ba0d48ca9472620e6bf3f4eb3003bedc6bf
uwee <uweenukr@gmail.com>
2018-12-18 12:15:01 -0500

    Added namespace Insight

--- Assets/Scripts/Insight/Core/InsightMessage.cs
+++ Assets/Scripts/Insight/Core/InsightMessage.cs
@@ -1,39 +1,41 @@
 ﻿using Mirror;
 
-public class EmptyReply : MessageBase
-{
+namespace Insight{
+    public class EmptyReply : MessageBase
+    {
 
-}
+    }
 
-public class RegisterServerConnectionMsg : MessageBase
-{
-    public static short MsgId = 1000;
-    public string UniqueID;
-}
+    public class RegisterServerConnectionMsg : MessageBase
+    {
+        public static short MsgId = 1000;
+        public string UniqueID;
+    }
 
-//Test msgs for Demo only
-public class ClientToZoneTestMsg : MessageBase
-{
-    public static short MsgId = 1000;
-    public string Source;
-    public string Desintation;
-    public string Data;
-}
+    //Test msgs for Demo only
+    public class ClientToZoneTestMsg : MessageBase
+    {
+        public static short MsgId = 1000;
+        public string Source;
+        public string Desintation;
+        public string Data;
+    }
 
-//Test msgs for Demo only
-public class ClientToMasterTestMsg : MessageBase
-{
-    public static short MsgId = 1001;
-    public string Source;
-    public string Desintation;
-    public string Data;
-}
+    //Test msgs for Demo only
+    public class ClientToMasterTestMsg : MessageBase
+    {
+        public static short MsgId = 1001;
+        public string Source;
+        public string Desintation;
+        public string Data;
+    }
 
-//Test msgs for Demo only
-public class ZoneToMasterTestMsg : MessageBase
-{
-    public static short MsgId = 1002;
-    public string Source;
-    public string Desintation;
-    public string Data;
+    //Test msgs for Demo only
+    public class ZoneToMasterTestMsg : MessageBase
+    {
+        public static short MsgId = 1002;
+        public string Source;
+        public string Desintation;
+        public string Data;
+    }
 }

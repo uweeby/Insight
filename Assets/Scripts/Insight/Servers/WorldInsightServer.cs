b7a2dede796cd84d8ba2c5f9a189abb6f0995811
uwee <uweenukr@gmail.com>
2018-12-18 20:36:29 -0500

    example world server

---
+++ Assets/Scripts/Insight/Servers/WorldInsightServer.cs
@@ -0,0 +1,21 @@
+﻿using Insight;
+
+public class WorldInsightServer : InsightServer
+{
+    // Use this for initialization
+    public override void Start ()
+    {        
+        RegisterHandlers();
+    }
+
+    // Update is called once per frame
+    public override void Update ()
+    {
+        HandleNewMessages();
+    }
+
+    private void RegisterHandlers()
+    {
+        
+    }
+}

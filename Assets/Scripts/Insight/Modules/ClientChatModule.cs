929dd2ff1c20d7f78f170dc87f71e557400a59f1
uwee <uweenukr@gmail.com>
2018-12-21 13:58:21 -0500

    updates to chat demo scene

---
+++ Assets/Scripts/Insight/Modules/ClientChatModule.cs
@@ -0,0 +1,19 @@
+﻿using Insight;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClientChatModule : InsightModule
+{
+    InsightCommon insight;
+
+    public override void Initialize(InsightCommon insight)
+    {
+        this.insight = insight;
+    }
+
+    public override void RegisterHandlers()
+    {
+        
+    }
+}

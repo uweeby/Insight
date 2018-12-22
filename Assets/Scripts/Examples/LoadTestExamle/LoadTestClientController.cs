65998c7a6cb985aeaa87a0f90e2f61d387c8bd1b
uwee <uweenukr@gmail.com>
2018-12-22 15:13:30 -0500

    load tester almost ready.

---
+++ Assets/Scripts/Examples/LoadTestExamle/LoadTestClientController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTestClientController : MonoBehaviour
+{
+	public GameObject InsightClientPrefab;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+
+	public void HandleNewClientButton()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Examples/LoadTestExamle/LoadTestModule.cs
@@ -0,0 +1,23 @@
+﻿using Insight;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadTestModule : InsightModule
+{
+    InsightCommon insight;
+
+    //Used in Example Scene:
+    
+    public override void Initialize(InsightCommon insight)
+    {
+        this.insight = insight;
+
+        RegisterHandlers();
+    }
+
+    public override void RegisterHandlers()
+    {
+
+    }
+}

44ee8025e873c34b0ff65b49aa5b11fedfe37b7c
uwee <uweenukr@gmail.com>
2018-12-22 14:49:35 -0500

    ArgsModule working in Spawner example

--- Assets/Scripts/Insight/Modules/ArgsModule.cs
+++ Assets/Scripts/Insight/Modules/ArgsModule.cs
@@ -7,15 +7,28 @@ public class ArgsModule : InsightModule
 {
     InsightCommon insight;
 
+    private InsightArgs insightArguments = new InsightArgs();
+
     public override void Initialize(InsightCommon insight)
     {
         this.insight = insight;
 
         RegisterHandlers();
+
+        SetupWithArgs();
     }
 
     public override void RegisterHandlers()
     {
         
+    }
+
+    private void SetupWithArgs()
+    {
+        if (insightArguments.IsProvided("-AssignedPort"))
+        {
+            insight.networkPort = insightArguments.ExtractValueInt("-AssignedPort");
+            Debug.Log("[ArgsModule] - Set Port: " + insight.networkPort);
+        }
     }
 }

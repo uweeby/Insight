890d32b6ea6e2577190647a3e0ab1c9fa0fd4634
uwee <uweenukr@gmail.com>
2018-12-22 14:32:50 -0500

    Starting on Args module

---
+++ Assets/Scripts/Insight/Modules/ArgsModule.cs
@@ -0,0 +1,21 @@
+﻿using Insight;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArgsModule : InsightModule
+{
+    InsightCommon insight;
+
+    public override void Initialize(InsightCommon insight)
+    {
+        this.insight = insight;
+
+        RegisterHandlers();
+    }
+
+    public override void RegisterHandlers()
+    {
+        
+    }
+}

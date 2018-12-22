9b78303f8d1a3edc3f5f86ce00fa3baece74c590
uwee <uweenukr@gmail.com>
2018-12-21 20:35:22 -0500

    Created empty modules for LoginExample

---
+++ Assets/Scripts/Examples/LoginExample/ClientLoginModule.cs
@@ -0,0 +1,18 @@
+﻿using Insight;
+
+public class ClientLoginModule : InsightModule
+{
+    InsightCommon insight;
+
+    public override void Initialize(InsightCommon insight)
+    {
+        this.insight = insight;
+
+        RegisterHandlers();
+    }
+
+    public override void RegisterHandlers()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Examples/LoginExample/ServerLoginModule.cs
@@ -0,0 +1,18 @@
+﻿using Insight;
+
+public class ServerLoginModule : InsightModule
+{
+    InsightCommon insight;
+
+    public override void Initialize(InsightCommon insight)
+    {
+        this.insight = insight;
+
+        RegisterHandlers();
+    }
+
+    public override void RegisterHandlers()
+    {
+        
+    }
+}

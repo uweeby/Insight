b8c3746f5dcd034196bea0431a25756f26e48baf
uwee <uweenukr@gmail.com>
2019-01-17 21:01:33 -0500

    WIP MatchMaking

---
+++ Assets/InsightExamples/MatchMaking/ServerMatchMakerModule.cs
@@ -0,0 +1,28 @@
+﻿using Insight;
+using UnityEngine;
+
+public class ServerMatchMakerModule : InsightModule
+{
+    InsightServer server;
+
+    //List of all players looking for a Match
+    //List of all games/spawners available to spawn a new Match
+
+    public override void Initialize(InsightServer insight, ModuleManager manager)
+    {
+        server = insight;
+        RegisterHandlers();
+    }
+
+    void RegisterHandlers()
+    {
+        server.RegisterHandler(FindMatch.MsgId, HandleFindMatch);
+    }
+
+    private void HandleFindMatch(InsightNetworkMessage netMsg)
+    {
+        FindMatch message = netMsg.ReadMessage<FindMatch>();
+
+
+    }
+}

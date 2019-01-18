03f391c2d66783da6e07ebd3634a88e9e3ef7eee
uwee <uweenukr@gmail.com>
2019-01-17 20:43:50 -0500

    WIP GameManager

---
+++ Assets/InsightExamples/GameManager/GameRegistration.cs
@@ -0,0 +1,51 @@
+﻿using Insight;
+using Mirror;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameRegistration : InsightModule
+{
+    InsightClient client;
+    public NetworkManager networkManager;
+
+    public List<GameContainer> registeredGames = new List<GameContainer>();
+
+    public override void Initialize(InsightClient insight, ModuleManager manager)
+    {
+        client = insight;
+
+        client.OnConnectedEvent.AddListener(ClientOnConnectedEventHandler);
+
+        RegisterHandlers();
+
+        networkManager = NetworkManager.singleton;
+    }
+
+    void RegisterHandlers()
+    {
+    }
+
+    private void ClientOnConnectedEventHandler()
+    {
+        //Gather Params
+        List<string> portArgs;
+        if(InsightArgs.TryGetArgument("NetworkPort", out portArgs))
+        {
+            networkManager.networkPort = Convert.ToUInt16(portArgs[0]);
+        }
+
+        //Apply necessary changes to NetworkManager
+
+        //Start NetworkManager
+
+        //Register back to the GameManager now that the game is running
+        SendGameRegistrationToGameManager();
+    }
+
+    private void SendGameRegistrationToGameManager()
+    {
+        Debug.Log("sending registration msg back to master");
+        client.Send(RegisterGame.MsgId, new RegisterGame() { UniqueID = Guid.NewGuid().ToString()}); //The GUID can/should be provided by the spawner for security
+    }
+}
---
+++ Assets/InsightExamples/GameManager/ServerGameManagerModule.cs
@@ -0,0 +1,85 @@
+﻿using Insight;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerGameManagerModule : InsightModule
+{
+    InsightServer server;
+    ModuleManager manager;
+    public MasterSpawner masterSpawner;
+
+    public List<GameContainer> registeredGames = new List<GameContainer>();
+
+    public void Awake()
+    {
+        AddDependency<MasterSpawner>();
+    }
+
+    public override void Initialize(InsightServer insight, ModuleManager manager)
+    {
+        server = insight;
+        this.manager = manager;
+        masterSpawner = manager.GetModule<MasterSpawner>();
+        RegisterHandlers();
+    }
+
+    void RegisterHandlers()
+    {
+        server.RegisterHandler(RegisterGame.MsgId, HandleRegisterGame);
+        server.RegisterHandler(GamesList.MsgId, HandleGamesList);
+    }
+
+    private void HandleRegisterGame(InsightNetworkMessage netMsg)
+    {
+        RegisterGame message = netMsg.ReadMessage<RegisterGame>();
+
+        if (server.logNetworkMessages) { Debug.Log("Received GameRegistration request"); }
+    
+
+        registeredGames.Add(new GameContainer() { connectionId = netMsg.connectionId, uniqueId = message.UniqueID });
+    }
+
+    private void HandleGamesList(InsightNetworkMessage netMsg)
+    {
+        GamesList message = netMsg.ReadMessage<GamesList>();
+
+        List<GameContainer> gamesMeetingCriteria = new List<GameContainer>();
+
+        //Check the local collection of Registered Games.
+        foreach(GameContainer game in registeredGames)
+        {
+            if(game.GameType.Equals("SomeAwesomeGame"))
+            {
+                gamesMeetingCriteria.Add(game);
+            }
+        }
+
+        //If a game meeting the options criteria is not found. Request a spawn.
+        if(gamesMeetingCriteria.Count == 0)
+        {
+            masterSpawner.RequestGameSpawn(); //Add the options here
+
+            //Should this also be a callback?
+
+            //Somehow the list needs to get populated
+        }
+
+        //Reply to fulfil the callback request.
+        netMsg.Reply(GamesList.MsgId, new GamesList() { }); //Send gamesMeetingCriteria back to fulfil callback.
+    }
+}
+
+public partial class MasterSpawner
+{
+    public void RequestGameSpawn() //Take in the options here
+    {
+
+    }
+}
+
+public struct GameContainer
+{
+    public string uniqueId;
+    public int connectionId;
+    public string GameType;
+}

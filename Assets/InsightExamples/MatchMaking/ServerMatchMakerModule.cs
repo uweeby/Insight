6f9105921cca5d59afc3b01bd34ed359fe36119a
uwee <uweenukr@gmail.com>
2019-01-18 13:25:20 -0500

    Added dependency on GameManager

--- Assets/InsightExamples/MatchMaking/ServerMatchMakerModule.cs
+++ Assets/InsightExamples/MatchMaking/ServerMatchMakerModule.cs
@@ -1,16 +1,27 @@
-﻿using Insight;
+﻿using System.Collections.Generic;
+using Insight;
 using UnityEngine;
 
 public class ServerMatchMakerModule : InsightModule
 {
     InsightServer server;
+    ModuleManager manager;
+    ServerGameManagerModule gameManager;
 
     //List of all players looking for a Match
-    //List of all games/spawners available to spawn a new Match
+    //List of all games. If a new game needs to be spawned it should ask the GameManager to do it.
+    List<UserSeekingMatch> usersSeekingMatchList = new List<UserSeekingMatch>();
+
+    public void Awake()
+    {
+        AddDependency<ServerGameManagerModule>();
+    }
 
     public override void Initialize(InsightServer insight, ModuleManager manager)
     {
         server = insight;
+        this.manager = manager;
+        gameManager = this.manager.GetModule<ServerGameManagerModule>();
         RegisterHandlers();
     }
 
@@ -23,6 +34,15 @@ public class ServerMatchMakerModule : InsightModule
     {
         FindMatch message = netMsg.ReadMessage<FindMatch>();
 
+        //Add player to list of players looking for a match
+        usersSeekingMatchList.Add(new UserSeekingMatch() { playerName = "Player01", gameType = "FFA" });
 
+    }
+
+    public struct UserSeekingMatch
+    {
+        //Placeholder
+        public string playerName;
+        public string gameType;
     }
 }

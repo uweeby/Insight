e44ac322fbe694458094611b9d47eb2d62f9f0e0
uwee <uweenukr@gmail.com>
2019-01-20 20:59:06 -0500

    renamed script

--- Assets/InsightExamples/5. MasterServer/MasterServerGUI.cs
+++ Assets/InsightExamples/5. MasterServer/MasterServerGUI.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class ActivatePlayerObject : MonoBehaviour
+public class MasterServerGUI : MonoBehaviour
 {
     public GameObject PlayerGameObject;
 

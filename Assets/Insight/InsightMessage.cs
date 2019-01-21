3dc2ebc2aa91e571373ec2a47d8f5b7a1e625fb4
uwee <uweenukr@gmail.com>
2019-01-20 20:16:25 -0500

    Moved some messages to InsightMessages as they are now stable

--- Assets/Insight/InsightMessage.cs
+++ Assets/Insight/InsightMessage.cs
@@ -2,21 +2,52 @@ using Mirror;
 
 namespace Insight
 {
-    public class EmptyReply : MessageBase
-    {
+    public enum MsgId : short
+    { 
+        Error = -1,
+        Empty,
+        Status,
 
-    }
+        Login,
+        Chat,
 
-    public class StatusMsg : MessageBase
-    {
-        public static short MsgId = -2;
-        public string Text;
+        RegisterSpawner,
+        RegisterGame,
     }
 
     public class ErrorMsg : MessageBase
     {
-        public static short MsgId = -1;
         public string Text;
         public bool CauseDisconnect;
+    }
+
+    public class EmptyMsg : MessageBase {}
+
+    public class StatusMsg : MessageBase
+    {
+        public string Text;
+    }
+
+    public class LoginMsg : MessageBase
+    {
+        public string AccountName;
+        public string AccountPassword;
+    }
+
+    public class ChatMsg : MessageBase
+    {
+        public string Data;
+        public string Origin;
+        public string Target; //Used for private chat
+    }
+
+    public class RegisterSpawnerMsg : MessageBase
+    {
+        public string UniqueID; //Guid
+    }
+
+    public class RegisterGameMsg : MessageBase
+    {
+        public string UniqueID; //Guid
     }
 }

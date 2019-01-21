b833e38b3cbe04b6451e35160caa599d065527be
uwee <uweenukr@gmail.com>
2019-01-20 20:18:26 -0500

    moved some msgs to InsightMessages main script

--- Assets/InsightExamples/ExampleMessages.cs
+++ Assets/InsightExamples/ExampleMessages.cs
@@ -1,120 +1,26 @@
 ﻿using Mirror;
-
-namespace Insight
+    
+//Test msg for SpawnerExample
+public class SpawnRequest : MessageBase
 {
-    //Test msg for ZoneModule
-    public class RegisterZoneMsg : MessageBase
-    {
-        public static short MsgId = 1003;
-        public string UniqueID;
-        public string SceneName;
-        public string NetworkAddress;
-        public int NetworkPort;
-        public int MaxPlayers;
-        public int CurentPlayers;
-    }
+    public static short MsgId = 1011;
+    public string GameName; //or SceneName
+    public string UniqueID;
+    public string NetworkAddress;
+    public int NetworkPort;
+}
 
-    //Test msg for ZoneModule
-    public class UnregisterZoneMsg : MessageBase
-    {
-        public static short MsgId = 1004;
-        public string UniqueID;
-    }
+//Test msg for GameManager
+public class GamesList : MessageBase
+{
+    public static short MsgId = 1014;
+    //OptionCollection GOES HERE
+}
 
-    //Test msg for ZoneModule
-    public class GetZonesMsg : MessageBase
-    {
-        public static short MsgId = 1005;
-        public ZoneContainer[] zonesList;
-    }
+//Test msg for MatchMaker
+public class FindMatch : MessageBase
+{
+    public static short MsgId = 1015;
 
-    //Test msg for ChatServer
-    public class ChatMessage : MessageBase
-    {
-        public static short MsgId = 1006;
-        public string Data;
-        public string Origin;
-        public string Target; //If this was a private chat
-    }
-
-    //Test msg for LoginServer
-    public class LoginMsg : MessageBase
-    {
-        public static short MsgId = 1007;
-        public string AccountName;
-        public string AccountPassword;
-        public string ClientVersion;
-    }
-
-    //Test msg for LoadTest
-    public class ServerLoadTestMsg : MessageBase
-    {
-        public static short MsgId = 1008;
-        public char[] Payload;
-    }
-
-    //Test msg for LoadTest
-    public class ClientLoadTestMsg : MessageBase
-    {
-        public static short MsgId = 1009;
-        public char[] Payload;
-    }
-
-    //Test msg for LoadTest
-    public class ServerHealthMsg : MessageBase
-    {
-        public static short MsgId = 1010;
-        public float CPULoadPercent;
-        public float RAMLoadPercent;
-        public float NETLoadPercent;
-    }
-
-    ////Test msg for SpawnerExample
-    //public class SpawnDataMessage : MessageBase
-    //{
-    //    public static short MsgId = 7003;
-    //    public string GameName; //or SceneName
-    //    public string UniqueID;
-    //    public string NetworkAddress;
-    //    public int NetworkPort;
-    //}
-
-    //Test msg for SpawnerExample
-    public class SpawnRequest : MessageBase
-    {
-        public static short MsgId = 1011;
-        public string GameName; //or SceneName
-        public string UniqueID;
-        public string NetworkAddress;
-        public int NetworkPort;
-    }
-
-    //Test msg for MasterServer
-    public class RegisterSpawner : MessageBase
-    {
-        public static short MsgId = 1012;
-        public string UniqueID;
-    }
-
-    //Test msg for GameManager
-    public class RegisterGame : MessageBase
-    {
-        public static short MsgId = 1013;
-        public string UniqueID;
-    }
-
-    //Test msg for GameManager
-    public class GamesList : MessageBase
-    {
-        public static short MsgId = 1014;
-        //OptionCollection GOES HERE
-    }
-
-    //Test msg for MatchMaker
-    public class FindMatch : MessageBase
-    {
-        public static short MsgId = 1015;
-
-        //options/param of Match they want to join
-    }
+    //options/param of Match they want to join
 }

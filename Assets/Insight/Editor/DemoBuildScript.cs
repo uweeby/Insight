f38652c86ed5867d6b3c2a941d7d270a79fb8732
uwee <uweenukr@gmail.com>
2019-01-28 13:17:43 -0500

    Moved build script and updated for new scenes

--- Assets/Insight/Editor/DemoBuildScript.cs
+++ Assets/Insight/Editor/DemoBuildScript.cs
@@ -2,11 +2,23 @@
 
 public class DemoBuildScript
 {
-    public static string ScenesRoot = "Assets/InsightExamples/";
+    public static string ScenesRoot = "Assets/Insight/Scenes/";
     public static BuildOptions BuildOptions = BuildOptions.Development;
     public static string PrevPath = null;
 
     #region Windows Builds
+    [MenuItem("Tools/Build/Windows/BasicGameServer", false, 999)]
+    public static void WindowsBuildAllMenu()
+    {
+        var path = GetPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            WindowsBuildBasicGameServer(path);
+            WindowsBuildManagedGameServer(path);
+            WindowsBuildChildSpawner(path);
+        }
+    }
+
     [MenuItem("Tools/Build/Windows/BasicGameServer", false, 999)]
     public static void WindowsBuildBasicGameServerMenu()
     {
@@ -27,13 +39,23 @@ public class DemoBuildScript
         }
     }
 
+    [MenuItem("Tools/Build/Windows/ManagedGameServer", false, 999)]
+    public static void WindowsBuildChildSpawnerMenu()
+    {
+        var path = GetPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            WindowsBuildChildSpawner(path);
+        }
+    }
+
     public static void WindowsBuildBasicGameServer(string path)
     {
         BuildTarget TargetPlatform = BuildTarget.StandaloneWindows;
 
         var gameServerScenes = new[]
         {
-            ScenesRoot+"4. Spawner/BasicGameServer.unity"
+            ScenesRoot+"BasicGameServer.unity"
         };
         PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
         PlayerSettings.productName = "BasicGameServer";
@@ -46,15 +68,40 @@ public class DemoBuildScript
 
         var gameServerScenes = new[]
         {
-            ScenesRoot+"6. GameManager/ManagedGameServer.unity"
+            ScenesRoot+"ManagedGameServer.unity"
         };
         PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
         PlayerSettings.productName = "ManagedGameServer";
         BuildPipeline.BuildPlayer(gameServerScenes, path + "/ManagedGameServer.exe", TargetPlatform, BuildOptions);
     }
+
+    public static void WindowsBuildChildSpawner(string path)
+    {
+        BuildTarget TargetPlatform = BuildTarget.StandaloneWindows;
+
+        var gameServerScenes = new[]
+        {
+            ScenesRoot+"ChildSpawner.unity"
+        };
+        PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
+        PlayerSettings.productName = "ChildSpawner";
+        BuildPipeline.BuildPlayer(gameServerScenes, path + "/ChildSpawner.exe", TargetPlatform, BuildOptions);
+    }
     #endregion
 
     #region Linux Builds
+    [MenuItem("Tools/Build/Linux/BasicGameServer", false, 999)]
+    public static void LinuxBuildAllMenu()
+    {
+        var path = GetPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            LinuxBuildBasicGameServer(path);
+            LinuxBuildManagedGameServer(path);
+            LinuxBuildChildSpawner(path);
+        }
+    }
+
     [MenuItem("Tools/Build/Linux/BasicGameServer", false, 999)]
     public static void LinuxBuildBasicGameServerMenu()
     {
@@ -75,13 +122,23 @@ public class DemoBuildScript
         }
     }
 
+    [MenuItem("Tools/Build/Linux/BasicGameServer", false, 999)]
+    public static void LinuxBuildChildSpawnerMenu()
+    {
+        var path = GetPath();
+        if (!string.IsNullOrEmpty(path))
+        {
+            LinuxBuildChildSpawner(path);
+        }
+    }
+
     public static void LinuxBuildBasicGameServer(string path)
     {
         BuildTarget TargetPlatform = BuildTarget.StandaloneLinux64;
 
         var gameServerScenes = new[]
         {
-            ScenesRoot+"4. Spawner/BasicGameServer.unity"
+            ScenesRoot+"BasicGameServer.unity"
         };
         PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
         PlayerSettings.productName = "BasicGameServer";
@@ -94,11 +151,24 @@ public class DemoBuildScript
 
         var gameServerScenes = new[]
         {
-            ScenesRoot+"6. GameManager/ManagedGameServer.unity"
+            ScenesRoot+"ManagedGameServer.unity"
         };
         PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
         PlayerSettings.productName = "ManagedGameServer";
         BuildPipeline.BuildPlayer(gameServerScenes, path + "/ManagedGameServer.x86_64", TargetPlatform, BuildOptions);
+    }
+
+    public static void LinuxBuildChildSpawner(string path)
+    {
+        BuildTarget TargetPlatform = BuildTarget.StandaloneLinux64;
+
+        var gameServerScenes = new[]
+        {
+            ScenesRoot+"ChildSpawner.unity"
+        };
+        PlayerSettings.displayResolutionDialog = ResolutionDialogSetting.Enabled;
+        PlayerSettings.productName = "ChildSpawner";
+        BuildPipeline.BuildPlayer(gameServerScenes, path + "/ChildSpawner.x86_64", TargetPlatform, BuildOptions);
     }
     #endregion
 

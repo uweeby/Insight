947f09800ee519ea224d78080be8f57a8376019b
uwee <uweenukr@gmail.com>
2019-02-06 20:15:50 -0500

    removed unused args for now

--- Assets/Insight/InsightArgs.cs
+++ Assets/Insight/InsightArgs.cs
@@ -13,32 +13,16 @@ public class InsightArgs
 
         Names = new ArgNames();
 
-        StartMaster = IsProvided(Names.StartMaster);
-        MasterPort = ExtractValueInt(Names.MasterPort, 5000);
-        MasterIp = ExtractValue(Names.MasterIp);
-        MachineIp = ExtractValue(Names.MachineIp);
-        DestroyUi = IsProvided(Names.DestroyUi);
-
-        AssignedPort = ExtractValueInt(Names.AssignedPort, -1);
+        NetworkAddress = ExtractValue(Names.NetworkAddress, "localhost");
+        NetworkPort = ExtractValueInt(Names.NetworkPort, 7777);
         UniqueID = ExtractValue(Names.UniqueID);
-        ExecutablePath = ExtractValue(Names.ExecutablePath);
-        DontSpawnInBatchmode = IsProvided(Names.DontSpawnInBatchmode);
-        MaxProcesses = ExtractValueInt(Names.MaxProcesses, 0);
         SceneName = ExtractValue(Names.SceneName);
     }
 
     #region Arguments
-    public bool StartMaster { get; private set; }
-    public int MasterPort { get; private set; }
-    public string MasterIp { get; private set; }
-    public string MachineIp { get; private set; }
-    public bool DestroyUi { get; private set; }
-
-    public int AssignedPort { get; private set; }
+    public string NetworkAddress { get; private set; }
+    public int NetworkPort { get; private set; }
     public string UniqueID { get; private set; }
-    public string ExecutablePath { get; private set; }
-    public bool DontSpawnInBatchmode { get; private set; }
-    public int MaxProcesses { get; private set; }
     public string SceneName { get; private set; }
     #endregion
 
@@ -67,19 +51,9 @@ public class InsightArgs
 
     public class ArgNames
     {
-        public string StartMaster { get { return "-StartMaster"; } }
-        public string MasterPort { get { return "-MasterPort"; } }
-        public string MasterIp { get { return "-MasterIp"; } }
-        public string MachineIp { get { return "-MachineIp"; } }
-        public string DestroyUi { get { return "-DestroyUi"; } }
-
-        public string StartSpawner { get { return "-StartSpawner"; } }
-
-        public string AssignedPort { get { return "-AssignedPort"; } }
+        public string NetworkAddress { get { return "-NetworkAddress"; } }
+        public string NetworkPort { get { return "-NetworkPort"; } }
         public string UniqueID { get { return "-UniqueID"; } }
-        public string ExecutablePath { get { return "-Exe"; } }
-        public string DontSpawnInBatchmode { get { return "-DontSpawnInBatchmode"; } }
-        public string MaxProcesses { get { return "-MaxProcesses"; } }
         public string SceneName { get { return "-SceneName"; } }
     }
 }

22127df413aa70eb2e7168b5b067248810672ea8
uwee <uweenukr@gmail.com>
2019-02-16 23:23:37 -0500

    moved game related code on client to ClientGameManager module

--- Assets/Insight/Modules/MatchMaking/ClientMatchMaking.cs
+++ Assets/Insight/Modules/MatchMaking/ClientMatchMaking.cs
@@ -1,21 +1,10 @@
-﻿using Mirror;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-//TODO: Remove the example specific code from module
+﻿using UnityEngine;
 
 namespace Insight
 {
     public class ClientMatchMaking : InsightModule
     {
         [HideInInspector] public InsightClient client;
-        public NetworkManager networkManager;
-        public TelepathyTransport transport;
-
-        public PlayerClientGUI playerGUI; //FIXME: Error when Examples are removed
-
-        public List<GameContainer> gamesList = new List<GameContainer>();
 
         public override void Initialize(InsightClient client, ModuleManager manager)
         {
@@ -26,53 +15,10 @@ namespace Insight
 
         void RegisterHandlers()
         {
-            client.RegisterHandler((short)MsgId.ChangeServers, HandleChangeServersMsg);
-            client.RegisterHandler((short)MsgId.GameList, HandleGameListMsg);
-        }
 
-        private void HandleChangeServersMsg(InsightNetworkMessage netMsg)
-        {
-            ChangeServerMsg message = netMsg.ReadMessage<ChangeServerMsg>();
-
-            if (client.logNetworkMessages) { Debug.Log("[InsightClient] - Connecting to GameServer: " + message.NetworkAddress + ":" + message.NetworkPort + "/" + message.SceneName); }
-
-            networkManager.networkAddress = message.NetworkAddress;
-            transport.port = message.NetworkPort;
-            SceneManager.LoadScene(message.SceneName);
-            networkManager.StartClient();
-        }
-
-        private void HandleGameListMsg(InsightNetworkMessage netMsg)
-        {
-            GameListMsg message = netMsg.ReadMessage<GameListMsg>();
-
-            if (client.logNetworkMessages) { Debug.Log("[InsightClient] - Received Games List"); };
-
-            gamesList.Clear();
-
-            foreach (GameContainer game in message.gamesArray)
-            {
-                Debug.Log(game.SceneName);
-
-                gamesList.Add(new GameContainer()
-                {
-                    UniqueId = game.UniqueId,
-                    SceneName = game.SceneName,
-                    CurrentPlayers = game.CurrentPlayers,
-                    MaxPlayers = game.MaxPlayers,
-                    MinPlayers = game.MinPlayers
-                });
-            }
-
-            playerGUI.UpdateGameListUI();
         }
 
         #region Message Senders
-        public void SendRequestSpawn(RequestSpawnMsg requestSpawnMsg)
-        {
-            client.Send((short)MsgId.RequestSpawn, requestSpawnMsg);
-        }
-
         public void SendStartMatchMaking(StartMatchMakingMsg startMatchMakingMsg)
         {
             client.Send((short)MsgId.StartMatchMaking, startMatchMakingMsg);
@@ -81,16 +27,6 @@ namespace Insight
         public void SendStopMatchMaking()
         {
             client.Send((short)MsgId.StopMatchMaking, new StopMatchMakingMsg());
-        }
-
-        public void SendJoinGameMsg(string UniqueID)
-        {
-            client.Send((short)MsgId.JoinGame, new JoinGamMsg() { UniqueID = UniqueID });
-        }
-
-        public void SendGetGameListMsg()
-        {
-            client.Send((short)MsgId.GameList, new GameListMsg());
         }
         #endregion
     }
